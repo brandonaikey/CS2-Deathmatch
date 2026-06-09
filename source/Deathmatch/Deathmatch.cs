b3d32c6fa6ef2870a8a5d6b2537d47d499e827f4
Brandon <brandon.aikey@gmail.com>
2026-06-09 16:21:38 -0500

    Remove BOM

--- source/Deathmatch/Deathmatch.cs
+++ source/Deathmatch/Deathmatch.cs
@@ -1,4 +1,4 @@
-﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Memory;
